f2d1b11b4fbd1abf189c935c923b1416267a7256
ravenhu1004 <43175869+ravenhu1004@users.noreply.github.com>
2018-12-04 14:06:52 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,57 +1,90 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=lantc_cinetrip;charset=utf8mb4";
-$dbusername = "lantc";
-$dbpassword = "NkXHus3h!6V";
+$dsn = "mysql:host=localhost;dbname=cinetrip;charset=utf8mb4";
+$dbusername = "huren";
+$dbpassword = "jam19901004";
 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>CineTrip Login</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="css/main.css" />
-    </head>
-    <body>
-        <header id="header" class="alt">
-              <a href = "homepage.php"><img src="assets/logo-01.png" alt="CineTrip Logo" style="width:100px"></a>
-              <nav id="nav">
-                  <ul>
-                      <li><a href="homepage.php">Home</a></li>
-                      <li><a href="search.php" class="icon fa-angle-down">Search</a></li>
-                      <li><a href="locations.php">Locations</a></li>
-                      <li><a href="about.php">About</a></li>
-                      <?php if($_SESSION['logged-in'] == true){?>
-                          <li><a href="userprofile.php" class="button">Profile</a></li>
-                          <li><a href="logout.php" class="button">Logout</a></li>
-                      <?php } else {?>
-                          <li><a href="login.php" class="button">Log in</a></li>
-                          <li><a href="signup.php" class="button">Sign up</a></li>
-                      <?php } ?>
-                  </ul>
-              </nav>
-          </header> 
-        <section>
+<head>
+	<title>CineTrip Login</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/organize.css">
+<!--	<link rel="stylesheet" type="text/css" href="css/small.css">-->
+	
+</head>
+<body>
+	<div class="top">
+		<a href="homepage.php" class="logo">
+      		<img src="assets/logo-03.png" />
+    	</a>
+		<div class="userImage">
+			<a href="signin.php">
+			<img src="assets/userimage.png" />
+			</a>
+		</div>
+		<div class="topnav">
+		<ul>
+        <?php if($_SESSION['logged-in'] == true){?>
+            <li><a href="logout.php">Log out</a></li>
+            <li><a href="userprofile.php">Profile</a></li>
+        <?php } else {?>
+            <li><a href="signup.php">Sign up</a></li>
+            <li><a href="login.php">Log in</a></li>
+        <?php } ?>			
+		</ul>
+		</div>
+		<div class="searchBar">  
+        	<form action="search-results.php" method="POST">
+            <input type = "text" name="filmsearch">
+            <button type="submit">
+			<img src="assets/search.png" href="locations.php"/>
+			</button>  
+        	</form>  
+    	</div>  
+	</div>
+	<header>
+		
+		<a href="homepage.php">
+			<img src="assets/logo-01.png" />
+		</a>
+		
+    	<nav>
+     	 <ul>
+        	<div></div>
+       		 <li class="current"><a href="homepage.php">Home</a></li>
+        	 <li><a href="locations.php">Locations</a></li>
+        	 <li><a href="search.php">Search</a></li>
+        	 <li><a href="about.php">About</a></li>
+        	 <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+	</header>
+
+		<div class="logForm">
             <h1>Welcome back</h1>
                 <form action="login-process.php" method="POST"> 
-                Username: <input type="text" name="username" />
-                Password: <input type="text" name="userpassword" />
+                <input type="text" placeholder="username" name="username" />
+                <input type="text" placeholder="password" name="userpassword" />
                 <input type="submit" />
             	</form>
-        </section>
-                <footer id="footer">
-                    <div id="footer_logo">
-                     <a href="homepage.php"><img src="assets/footer-logo.png" style="width:77px;height:28px"></a>
-                     </div>
-                    <ul class="icons">
-                        <li><a href="about.php" ><span class="label">About CineTrip</span></a></li>
-                        <li><a href="#" ><span class="label">Contribute</span></a></li>
-                        <li><a href="#" ><span class="label">Privacy policy</span></a></li>
-                    </ul>
-                    <ul class="copyright">
-                        <li>&copy; CineTrip. All rights reserved.</li>
-                    </ul>
-                </footer>       
+        </div>
+	
+	
+                <div class="footer">
+   <footer>
+        <a href="homepage.php">
+          <img src="assets/footer-logo.png" />
+        </a>               
+    <ul class="bottomNav">
+        <li><a href="about.php" >About CineTrip</a></li>
+        <li><a href="#">Contribute</a></li>
+        <li><a href="#">Privacy policy</a></li>
+    </ul>
+    </footer>
+	</div>       
     </body>
 </html>
