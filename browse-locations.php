bea5271d600ddcfc4ab94e2edf4b15b5a9de43fb
wysockca <wysockca@users.noreply.github.com>
2018-12-05 12:03:51 -0500

    Update browse-locations.php

--- browse-locations.php
+++ browse-locations.php
@@ -16,7 +16,7 @@ $stmt->execute();
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Homepage</title>
+	<title>Cinetrip - Browse</title>
 	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" type="text/css" href="css/small.css">
