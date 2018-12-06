5821767044514fa023fa5a8a33ac1c5298189f25
ravenhu1004 <43175869+ravenhu1004@users.noreply.github.com>
2018-12-05 20:02:16 -0500

    Update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -16,63 +16,101 @@ $dbpassword = "NkXHus3h!6V";
 	<html>
 		<head>
 			<title>CineTrp My Profile</title>
-			<link rel="stylesheet" href="css/main.css" />
-		</head>
-	<body>
-        <header id="header" class="alt">
-              <a href = "homepage.php"><img src="assets/logo-01.png" alt="CineTrip Logo" style="width:100px"></a>
-              <nav id="nav">
-                  <ul>
-                      <li><a href="homepage.php">Home</a></li>
-                      <li><a href="search.php" class="icon fa-angle-down">Search</a></li>
-                      <li><a href="browse-locations.php">Locations</a></li>
-                      <li><a href="about.php">About</a></li>
-                      <?php if($_SESSION['logged-in'] == true){?>
-                          <li><a href="userprofile.php" class="button">Profile</a></li>
-                          <li><a href="logout.php" class="button">Logout</a></li>
-                      <?php } else {?>
-                          <li><a href="login.php" class="button">Log in</a></li>
-                          <li><a href="signup.php" class="button">Sign up</a></li>
-                      <?php } ?>
-                  </ul>
-              </nav>
-          </header>
-	<h1> User Profile</h1>
-			<a href="images-profile/profile_01.png"/><img src="images-profile/profile_01.png" alt="Profile Photo" width="120" height="120" border="1"></a><br>
-
-<div class=>
-Username<br><br>
-Age<br><br>
-Favourite Genres<br><br>
-</div><br>
-
-<h2>Badges earned</h2><br>
-	<div class=>
-
-		<img src="images-profile/01_badge_horror.jpg" alt="Horror" width="80" height="80" border="1">
-		<img src="images-profile/02_badge_scifi.jpg" alt="Sci-Fi" width="80" height="80" border="1">
-		<img src="images-profile/03_badge_romance.jpg" alt="Romance" width="80" height="80" border="1">
-		<img src="images-profile/04_badge_comedy.jpg" alt="Comedy" width="80" height="80" border="1">
-		<img src="images-profile/05_badge_drama.jpg" alt="Drama" width="80" height="80" border="1">
-		<img src="images-profile/06_badge_fan.jpg" alt="Fan" width="80" height="80" border="1">
-		<img src="images-profile/07_badge_superfan.jpg" alt="Superfan" width="80" height="80" border="1">
-		<img src="images-profile/08_badge_filmbuff.jpg" alt="Film Buff" width="80" height="80" border="1">
-		<img src="images-profile/09_badge_expert.jpg" alt="Expert" width="80" height="80" border="1">
-		<img src="images-profile/10_badge_director.jpg" alt="Director" width="80" height="80" border="1">
+			<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	        <link rel="stylesheet" type="text/css" href="css/main.css">
+			<link rel="stylesheet" type="text/css" href="css/organize.css">
+	
+       </head>
+<body>
+	<div class="top">
+		<a href="homepage.php" class="logo">
+      		<img src="assets/logo-03.png" />
+    	</a>
+		<div class="userImage">
+			<a href="signin.php">
+			<img src="assets/userimage.png" />
+			</a>
+		</div>
+		<div class="topnav">
+		<ul>
+        <?php if(isset($_SESSION['logged-in']) && ($_SESSION['logged-in'] == true)){?>
+           <li><a href="logout.php">Log out</a></li>
+           <li><a href="userprofile.php">Profile</a></li>
+       <?php } else {?>
+           <li><a href="signup.php">Sign up</a></li>
+           <li><a href="login.php">Log in</a></li>
+       <?php } ?>		
+		</ul>
+		</div>
+		<div class="searchBar">  
+        	<form action="search-results.php" method="POST">
+            <input type = "text" name="filmsearch">
+            <button type="submit">
+			<img src="assets/search.png" href="locations.php"/>
+			</button>  
+        	</form>  
+    	</div>  
 	</div>
-                <footer id="footer">
-                    <div id="footer_logo">
-                     <a href="homepage.php"><img src="assets/footer-logo.png" style="width:77px;height:28px"></a>
-                     </div>
-                    <ul class="icons">
-                        <li><a href="about.php" ><span class="label">About CineTrip</span></a></li>
-                        <li><a href="#" ><span class="label">Contribute</span></a></li>
-                        <li><a href="#" ><span class="label">Privacy policy</span></a></li>
-                    </ul>
-                    <ul class="copyright">
-                        <li>&copy; CineTrip. All rights reserved.</li>
-                    </ul>
-                </footer>
+	<header>
+		
+		<a href="homepage.php">
+			<img src="assets/logo-01.png" />
+		</a>
+		
+    	<nav>
+     	 <ul>
+        	<div></div>
+       		 <li class="current"><a href="homepage.php">Home</a></li>
+        	 <li><a href="browse-locations.php">Locations</a></li>
+        	 <li><a href="search.php">Search</a></li>
+        	 <li><a href="about.php">About</a></li>
+        	 <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+	</header>
+	
+	<div class="profileSec">
+	<h1> User Profile</h1>
+	
+
+     <div class="info">
+	 <a href="images-profile/profile_01.png"/>
+		<img src="images-profile/profile_01.png" alt="Profile Photo">
+	 </a><br>
+	 
+     <p>Username<br><br>Age<br><br>Favourite Genres</p>
+	
+	 
+	 </div><br>
+
+	 <h2>Badges earned</h2><br>
+	 <div class="badges">
+
+		<img src="images-profile/01_badge_horror.jpg" alt="Horror" >
+		<img src="images-profile/02_badge_scifi.jpg" alt="Sci-Fi" />
+		<img src="images-profile/03_badge_romance.jpg" alt="Romance" />
+		<img src="images-profile/04_badge_comedy.jpg" alt="Comedy" />
+		<img src="images-profile/05_badge_drama.jpg" alt="Drama" />
+		<img src="images-profile/06_badge_fan.jpg" alt="Fan" />
+		<img src="images-profile/07_badge_superfan.jpg" alt="Superfan" />
+		<img src="images-profile/08_badge_filmbuff.jpg" alt="Film Buff" />
+		<img src="images-profile/09_badge_expert.jpg" alt="Expert" />
+		<img src="images-profile/10_badge_director.jpg" alt="Director" />
+	</div>
+	
+  </div>	
+   <div class="footer">
+   <footer>
+        <a href="homepage.php">
+          <img src="assets/footer-logo.png" />
+        </a>               
+    <ul class="bottomNav">
+        <li><a href="about.php" >About CineTrip</a></li>
+        <li><a href="#">Contribute</a></li>
+        <li><a href="#">Privacy policy</a></li>
+    </ul>
+    </footer>
+	</div>
 </body>
 </html>
 
