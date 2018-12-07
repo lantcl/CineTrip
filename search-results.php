7503469f15171e35462ca9f6ed0cee7b6fffd065
ravenhu1004 <43175869+ravenhu1004@users.noreply.github.com>
2018-12-06 21:11:02 -0500

    Update search-results.php

--- search-results.php
+++ search-results.php
@@ -26,55 +26,35 @@ $filmSearch->execute();
     <head>
         <title>Search Results</title>
         <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- <link rel="stylesheet" type="text/css" href="css/main.css"> -->
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/organize.css">
+        
     </head>
 <body>
-  <div class="top">
-    <a href="homepage.php" class="logo">
-          <img src="assets/logo-03.png" />
-      </a>
-    <div class="userImage">
-      <a href="signin.php">
-      <img src="assets/userimage.png" />
-      </a>
-    </div>
-    <div class="topnav">
-    <ul>
-        <?php if($_SESSION['logged-in'] == true){?>
-            <li><a href="logout.php" class="button">Log out</a></li>
-            <li><a href="userprofile.php" class="button">Profile</a></li>
-        <?php } else {?>
-            <li><a href="signup.php" class="button">Sign up</a></li>
-            <li><a href="login.php" class="button">Log in</a></li>
-        <?php } ?>      
-    </ul>
-    </div>
-    <div class="searchBar">  
-          <form action="search-results.php" method="POST">
-            <input type = "text" name="filmsearch">
-            <button type="submit">
-      <img src="assets/search.png" href="locations.php"/>
-      </button>  
-          </form>  
-      </div>  
-  </div>
   <header>
-    
-    <a href="homepage.php">
-      <img src="assets/logo-01.png" />
-    </a>
-    
-      <nav>
-       <ul>
-          <div></div>
-           <li class="current"><a href="homepage.php">Home</a></li>
-           <li><a href="locations.php">Locations</a></li>
-           <li><a href="search.php">Search</a></li>
-           <li><a href="about.php">About</a></li>
-           <li><a href="contact.php">Contact</a></li>
+		
+		<div>
+			<img src="assets/logo-01.png" href="homepage.php" class="logo"/>
+		</div>
+		
+    	<nav>
+     	 <ul>
+        	<div></div>
+       		 <li class="current"><a href="homepage.php">Home</a></li>
+        	 <li><a href="browse-locations.php">Locations</a></li>
+        	 <li><a href="search.php">Search</a></li>
+        	 <li><a href="about.php">About</a></li>
+        	 <li><a href="contact.php">Contact</a></li>
+		 <?php if($_SESSION['logged-in'] == true){?>
+                 <li><a href="logout.php">Log out</a></li>
+                 <li><a href="userprofile.php">Profile</a></li>
+                 <?php } else {?>
+                 <li><a href="signup.php">Sign up</a></li>
+                 <li><a href="login.php">Log in</a></li>
+                 <?php } ?>	
         </ul>
     </nav>
-  </header>
+	</header>
         <div class="rls">
             <?php 
         if (isset($_POST['filmsearch'])){ ?>
