4139e4ed4c1990e17076231b92e8e253d3ffa945
ravenhu1004 <43175869+ravenhu1004@users.noreply.github.com>
2018-12-06 21:06:32 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -20,40 +20,11 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
 	
 </head>
 <body>
-	<div class="top">
-		<a href="homepage.php" class="logo">
-      		<img src="assets/logo-03.png" />
-    	</a>
-		<div class="userImage">
-			<a href="signin.php">
-			<img src="assets/userimage.png" />
-			</a>
-		</div>
-		<div class="topnav">
-		<ul>
-        <?php if($_SESSION['logged-in'] == true){?>
-            <li><a href="logout.php">Log out</a></li>
-            <li><a href="userprofile.php">Profile</a></li>
-        <?php } else {?>
-            <li><a href="signup.php">Sign up</a></li>
-            <li><a href="login.php">Log in</a></li>
-        <?php } ?>			
-		</ul>
-		</div>
-		<div class="searchBar">  
-        	<form action="search-results.php" method="POST">
-            <input type = "text" name="filmsearch">
-            <button type="submit">
-			<img src="assets/search.png" href="locations.php"/>
-			</button>  
-        	</form>  
-    	</div>  
-	</div>
 	<header>
 		
-		<a href="homepage.php">
-			<img src="assets/logo-01.png" />
-		</a>
+		<div>
+			<img src="assets/logo-01.png" href="homepage.php" class="logo"/>
+		</div>
 		
     	<nav>
      	 <ul>
@@ -63,9 +34,17 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
         	 <li><a href="search.php">Search</a></li>
         	 <li><a href="about.php">About</a></li>
         	 <li><a href="contact.php">Contact</a></li>
+		 <?php if($_SESSION['logged-in'] == true){?>
+                 <li><a href="logout.php">Log out</a></li>
+                 <li><a href="userprofile.php">Profile</a></li>
+                 <?php } else {?>
+                 <li><a href="signup.php">Sign up</a></li>
+                 <li><a href="login.php">Log in</a></li>
+                 <?php } ?>	
         </ul>
     </nav>
 	</header>
+	
 	<div class="contact">
 		<div id="contactForm">
 			<label>Full name:</label>
