b61469346e47c67bf8111501091d5394be603837
ravenhu1004 <43175869+ravenhu1004@users.noreply.github.com>
2018-12-08 16:28:33 -0500

    Update profile-edit.php

--- profile-edit.php
+++ profile-edit.php
@@ -18,13 +18,104 @@ $dbpassword = "NkXHus3h!6V";
 <html>
 	<head>
 		<title>CineTrip - Edit Profile</title>
-		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="Cache-Control" content="no-transform" /> 
+		<meta http-equiv="Cache-Control" content="no-siteapp" />
+		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+	    <link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/organize.css">
 	
-		<link rel="stylesheet" type="text/css" href="css/main.css" />
-		<link rel="stylesheet" type="text/css" href="css/organize.css" />
-	
-<!--	<link rel="stylesheet" type="text/css" href="css/small.css">-->
-	
+        <style type="text/css">
+			@media screen and (max-width:960px){
+				
+				.proEdit{
+	width: 100%;
+	height: 100%;
+	overflow: auto;
+	background-color: #EBEBEB;
+	display:block; 
+	text-align:center;
+/*	margin-top: -1%;*/
+}
+
+.proEdit h1{
+	font-family: "Arial Rounded Mt",arial;
+	font-size: 2.8em;
+	font-color:#2e2e2e;
+	padding-top: 3%;
+	padding-bottom: 3%; 
+	text-align: center;
+	margin:0 auto;
+}
+
+.proEdit .editInfo{
+	background-color:rgba(255,255,255,.8);
+    width:85%;
+	height: 90%;
+	margin-top:0;
+    margin-left: 1%;
+    margin-right: 1%;
+    margin-bottom: 3%;
+    display:inline-block;
+    vertical-align: top;
+	text-align: center;
+	border-radius: 3px;
+}
+
+.proEdit .editInfo img{
+	width: 30%;
+	margin: 10%;
+}
+
+.editInfo p3{
+	font-family: "Arial Rounded Mt",arial;
+	font-size: 1em;
+	font-color:#2e2e2e;
+	padding-top: 1%;
+	padding-bottom: 1%; 
+	text-align: center;
+	margin:0 auto;
+}
+
+.editInfo input{
+	width:40%;
+    height: 15%;
+    border: 1em #AEAEAE;  
+    border-radius: 5px;  
+    background: #E2E2E2;  
+    color: #d9d9d9;
+	font-family: "Arial Rounded Mt",arial;
+	font-size: 1em;
+	padding-left: 1%;
+	margin: 4%;
+}
+
+.proEdit .editInfo input[type='submit'] {
+	        font-family: "Arial Rounded Mt",arial;
+            font-size: 1em;
+            color: #ffffff;
+            background-color: #ffab17;
+	        border-radius: 5px;
+	        margin:0 auto;
+}
+
+.editBadges img{
+	width:15%;
+	margin-top: .5%;
+	margin-bottom: 5%;
+}
+
+.proEdit .editBadges h2{
+	font-family: "Arial Rounded Mt",arial;
+	font-size: 2.8em;
+	font-color:#2e2e2e;
+	padding-top: 3%;
+	padding-bottom:0; 
+	text-align: center;
+	margin:0 auto;
+}
+
+			}
+		</style>
 </head>
 <body>
 	<header>
