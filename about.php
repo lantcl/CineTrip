89e4d52d637c5bd072d78686ea711a1cdcb99603
ravenhu1004 <43175869+ravenhu1004@users.noreply.github.com>
2018-12-09 15:08:57 -0500

    Update about.php

--- about.php
+++ about.php
@@ -13,6 +13,7 @@ $_SESSION['logged-in'] == true;
 		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 	    <link rel="stylesheet" type="text/css" href="css/main.css">
 		<link rel="stylesheet" type="text/css" href="css/organize.css">
+	    <link rel="stylesheet" media="screen and (max-width:960px)" href="css/small.css">
 	
         <style type="text/css">
 		.aboutInner img{
